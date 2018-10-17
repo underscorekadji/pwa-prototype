80112436363ba33afe1fdcca257895df8c5d0cfc
Alexander Belenkov <belenkov.alexander@gmail.com>
2018-10-17 12:50:55 +0300

    Updates

---
+++ precache-manifest.4eefbc4ede2fa8f1cb34329558bb23eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-prototype/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "22e3b85a4be87df52a09",
+    "url": "/pwa-prototype/static/js/runtime~main.22e3b85a.js"
+  },
+  {
+    "revision": "db70cb9953a9a8123498",
+    "url": "/pwa-prototype/static/js/main.db70cb99.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/pwa-prototype/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "db70cb9953a9a8123498",
+    "url": "/pwa-prototype/static/css/main.4e1f8559.chunk.css"
+  },
+  {
+    "revision": "9ea1ce08dc5979f71334ef1dfbaeff3a",
+    "url": "/pwa-prototype/index.html"
+  }
+];
